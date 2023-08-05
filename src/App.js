bcd6d4d38858f14c3848649f0a47abeab99a75bc
solahidris <emailsolah@gmail.com>
2023-08-06 00:10:51 +0800

    tolong jakz haha

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ function App() {
   return (
     <div className="bg-slate-700 min-h-[100vh]">
       <p>this is the pr branch</p>
+      <p>---</p>
+      <p>pull and push for jakz to review</p>
     </div>
   );
 }
