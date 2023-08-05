79706cd070235922b31e2a279b0b0ea1da7414d5
solahidris <emailsolah@gmail.com>
2023-08-05 22:33:07 +0800

    pr branch

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 function App() {
   return (
     <div className="bg-slate-700 min-h-[100vh]">
-      <p>this is the main branch</p>
+      <p>this is the pr branch</p>
     </div>
   );
 }
