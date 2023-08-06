0fd7c62c27eb26ae5b782969882bd86da0096dd9
solahidris <emailsolah@gmail.com>
2023-08-06 20:34:55 +0800

    Metamask connection successful

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -7,7 +7,6 @@ const Homepage = () => {
     return (
         <div className="bg-slate-700 min-h-[100vh]">
             <p className="text-xs text-white">this is the main branch</p>
-            {/* <Balance /> */}
             <Metamask />
         </div>
     )
--- src/components/Metamask.js
+++ src/components/Metamask.js
@@ -1,44 +1,93 @@
-// Metamask.js
-
-// Import everything
-import { ethers, JsonRpcProvider, getBlockNumber } from "ethers";
-
-// Import just a few select items
-// import { BrowserProvider, parseUnits } from "ethers";
-
-// Import from a specific export
-// import { HDNodeWallet } from "ethers/wallet";
+import React, { useEffect, useState, useRef } from "react";
+import { ethers, formatEther } from "ethers";
 
 const Metamask = () => {
+  const [signer, setSigner] = useState(null);
+  const [blockNumber, setBlockNumber] = useState(null);
+  const [balance, setBalance] = useState(null);
+  const [transactionCount, setTransactionCount] = useState(null);
+  const providerRef = useRef(null);
 
-  const apikey = process.env.INFURA_API_KEY;
+  const windowEthereum = async () => {
+    let provider;
+    if (!window.ethereum) {
+      console.log("MetaMask not installed; using read-only defaults");
+      provider = ethers.getDefaultProvider();
+    } else {
+      provider = new ethers.BrowserProvider(window.ethereum);
+      try {
+        const signerInstance = await provider.getSigner();
+        setSigner(signerInstance);
+      } catch (error) {
+        console.error("Error getting signer", error);
+      }
+    }
+    // Provider ref for all the other functions
+    providerRef.current = provider;
+  };
 
-  var ethers = require("ethers");
-  var url = `https://mainnet.infura.io/v3/${apikey}`;
-  var customHttpProvider = new ethers.providers.JsonRpcProvider(url);
-  customHttpProvider.getBlockNumber().then((result) => {
-    console.log("Current block number: " + result);
-  });
+  const blockNumberFn = async () => {
+    if (providerRef.current) {
+      const provider = providerRef.current;
+      // Look up the current block number (i.e. height)
+      const signersBlock = await provider.getBlockNumber();
+      setBlockNumber(signersBlock);
+      // 17831911
+    } else {
+      console.log("signer not assigned", "signerError");
+    }
+  };
+  const balanceFn = async () => {
+    if (providerRef.current) {
+      const provider = providerRef.current;
+      let address = signer.address;
+      // Get the current balance of an account (by address or ENS name)
+      const signersBalance = await provider.getBalance(address);
+      // Since the balance is in wei, format to display it in ether
+      setBalance(formatEther(signersBalance));
+    } else {
+      console.log("balance not assigned", "balanceError");
+    }
+  };
+  const transactionCountFn = async () => {
+    if (providerRef.current) {
+      const provider = providerRef.current;
+      // Get the next nonce required to send a transaction
+      const signersTransaction = await provider.getTransactionCount(
+        signer.address
+      );
+      setTransactionCount(signersTransaction);
+      // 3
+    } else {
+      console.log("transaction not assigned", "transactionCountError");
+    }
+  };
+
+  useEffect(() => {
+    windowEthereum();
+  }, []);
+
+  useEffect(() => {
+    if (signer) {
+      windowEthereum();
+      blockNumberFn();
+      balanceFn();
+      transactionCountFn();
+      // console.log(balance)
+    }
+  }, [signer]);
 
   return (
-    <div>
-      <h1>metamask page</h1>
+    <div className="w-80 mx-auto text-white">
+      <h1 className="text-center py-4 my-4 rounded-lg tracking-widest font-bold bg-slate-500/90">Metamask Page</h1>
+      <div className="p-4 rounded-lg bg-slate-500/90 leading-8 tracking-widest text-xs flex flex-col">
+        <h3 className="truncate hover:truncate-none">Signer:{signer?.address}</h3>
+        <h3>BlockNumber: {blockNumber}</h3>
+        <h3>Balance: {balance?.toString()}</h3>
+        <h3>Transaction Count: {transactionCount}</h3>
+      </div>
     </div>
   );
 };
 
 export default Metamask;
-
-// Ethers
-// Save the following script to a file, e.g. index.js
-
-// var ethers = require("ethers");
-// var url = "https://mainnet.infura.io/v3/<API-KEY>";
-// var customHttpProvider = new ethers.providers.JsonRpcProvider(url);
-// customHttpProvider.getBlockNumber().then((result) => {
-//   console.log("Current block number: " + result);
-// });
-
-// In a terminal window, run the script with node index.js
-
-// Latest Ethereum Block is  14659509
